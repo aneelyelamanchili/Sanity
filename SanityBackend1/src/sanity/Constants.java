0c133c7e93bfe42a2ce7410e9b0aa1b90606d9d2
davidsealand <sealand@usc.edu>
2017-10-25 23:59:12 -0700

    pen 15

--- SanityBackend1/src/sanity/Constants.java
+++ SanityBackend1/src/sanity/Constants.java
@@ -2,6 +2,9 @@ package sanity;
 
 public class Constants {
 	public static final String SQL_INSERT_USER = "INSERT INTO TotalUsers(FirstName, Lastname, Password, Email) VALUES ";
+	public static final String SQL_INSERT_BIGBUDGET = "INSERT INTO BigBudget(userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent)";
+	public static final String SQL_INSERT_BUDGET = "INSERT INTO Budget(budgetID, BudgetAmount, BudgetName)";
+	public static final String SQL_EDIT_BIGBUDGET = "UPDATE BigBudget SET ";
 	public static final String SQL_INSERT_RIDE = "INSERT INTO CurrentTrips(userID, FirstName, LastName, Email, StartingPoint, DestinationPoint, CarModel, LicensePlate, Cost, `Date/Time`, Detours, Hospitality, Food, Luggage, TotalSeats, SeatsAvailable) VALUES";
 	public static final String SQL_INSERT_PREVIOUSRIDE = "INSERT INTO TotalPreviousTrips(rideID, userID, FirstName, LastName, Email, StartingPoint, DestinationPoint, CarModel, LicensePlate, Cost, `Date/Time`, Detours, Hospitality, Food, Luggage, TotalSeats, SeatsFilled) VALUES";
 }

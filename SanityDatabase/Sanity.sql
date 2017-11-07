615c23bc04cd9792b3810fed251a5fc376b35a57
Adam Espinoza <adespinoza97@gmail.com>
2017-11-06 18:08:56 -0800

    Backend changes

--- SanityDatabase/Sanity.sql
+++ SanityDatabase/Sanity.sql
@@ -42,10 +42,11 @@ CREATE TABLE `Budgets` (
 CREATE TABLE `Transactions` (
 	`transactionID` INT(11) auto_increment NOT NULL,
     `budgetID` INT(11) NOT NULL,
-    `Amount` FLOAT NOT NULL,
+    `Amount` VARCHAR(50) NOT NULL,
     `Details` VARCHAR(50) NOT NULL,
 	`Latitude` FLOAT NOT NULL,
     `Longitude` FLOAT NOT NULL,
+    `DateValue` VARCHAR(50) NOT NULL,
     FOREIGN KEY(`budgetID`) REFERENCES `Budgets`(`budgetID`),
     PRIMARY KEY(`transactionID`)
 );
@@ -54,3 +55,5 @@ CREATE TABLE `Transactions` (
  INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft) VALUES (1, 'AneelLife', 2, 10, 10, 100000, 0, 10, 'date', 2);
  INSERT INTO `Budgets` (bigBudgetID, BudgetAmount, BudgetName, TotalAmountSpent) VALUES (1, 50000, 'Credit Cards', 0);
  INSERT INTO `Budgets` (bigBudgetID, BudgetAmount, BudgetName, TotalAmountSpent) VALUES (1, 50000, 'Ultimate Succ', 0);
+ INSERT INTO `Transactions`(transactionID, budgetID, Amount, Details, Latitude, Longitude, DateValue) VALUES(1, 1, '+500.12', 'Eating Ass', 33.0224,117.2851, '12/30/2016');
+  INSERT INTO `Transactions`(transactionID, budgetID, Amount, Details, Latitude, Longitude, DateValue) VALUES(2, 1, '-20.29', 'Sucking Tommy Trojan', 34.0224,118.2851, '11/20/2017');

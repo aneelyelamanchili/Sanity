a75c4fba8a9b98bf766b40295af48b00bd41b401
davidsealand <sealand@usc.edu>
2017-11-27 21:31:27 -0800

    huehue

--- SanityDatabase/Sanity.sql
+++ SanityDatabase/Sanity.sql
@@ -50,6 +50,7 @@ CREATE TABLE `Transactions` (
 	`Latitude` FLOAT NOT NULL,
     `Longitude` FLOAT NOT NULL,
     `DateValue` VARCHAR(50) NOT NULL,
+    `Automatic` INT(11) NOT NULL,
     FOREIGN KEY(`budgetID`) REFERENCES `Budgets`(`budgetID`),
     PRIMARY KEY(`transactionID`)
 );
@@ -65,18 +66,18 @@ CREATE TABLE `History` (
     PRIMARY KEY(`historyID`)
 );
 
---  INSERT INTO `TotalUsers`(FirstName, LastName, Password, Email) VALUES ('Aneel', 'Yelamanchili', 314, 'a');
---  INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'AneelLife', 2, 10, 10, 100, 0, 10, '11/13/2017', 0, '50 25','','80 90 95');
---  INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'Life', 2, 10, 10, 100, 40, 10, '11/13/2017', 0, '50 25','','80 90 95');
+  INSERT INTO `TotalUsers`(FirstName, LastName, Password, Email) VALUES ('Aneel', 'Yelamanchili', 314, 'a');
+  INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'AneelLife', 2, 10, 10, 100, 0, 10, '11/13/2017', 0, '50 25','','80 90 95');
+  INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'Life', 2, 10, 10, 100, 40, 10, '11/13/2017', 0, '50 25','','80 90 95');
   
--- INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'Annual Savings', 1, 0, 0, 0, 0, 365, '06/29/2018', 365, '50','','80 90 95');
--- INSERT INTO `Budgets` (bigBudgetID, BudgetAmount, BudgetName, TotalAmountSpent) VALUES (1, 100, 'Credit Cards', 60);
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 20, 1, '05/13/2017');
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 10, 2, '06/13/2017');
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 30, 3, '07/13/2017');
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 40, 4, '08/13/2017');
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 50, 5, '09/13/2017');
--- INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 60, 6, '10/13/2017');
+ INSERT INTO `BigBudgets` (userID, BigBudgetName, BarGraphColor, Latitude, Longitude, BigBudgetAmount, TotalAmountSpent, Frequency, Date, BigBudgetDaysLeft, PeriodNotification, PeriodNotificationChecked, LimitNotification) VALUES (1, 'Annual Savings', 1, 0, 0, 0, 0, 365, '06/29/2018', 365, '50','','80 90 95');
+ INSERT INTO `Budgets` (bigBudgetID, BudgetAmount, BudgetName, TotalAmountSpent) VALUES (1, 100, 'Credit Cards', 60);
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 20, 1, '05/13/2017');
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 10, 2, '06/13/2017');
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 30, 3, '07/13/2017');
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 40, 4, '08/13/2017');
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 50, 5, '09/13/2017');
+ INSERT INTO `History` (budgetID, CategoryAmount, TotalAmountSpent, HistoryNum, StartDate) VALUES (1, 100, 60, 6, '10/13/2017');
  -- INSERT INTO `Budgets` (bigBudgetID, BudgetAmount, BudgetName, TotalAmountSpent) VALUES (1, 50000, 'Ultimate Succ', 0);
   -- INSERT INTO `Transactions`(transactionID, budgetID, Amount, Details, Latitude, Longitude, DateValue) VALUES(1, 1, 500.12, 'Eating Food', 33.0224,117.2851, '12/30/2016');
 -- INSERT INTO `Transactions`(transactionID, budgetID, Amount, Details, Latitude, Longitude, DateValue) VALUES(2, 1, '-20.29', 'Tommy Trojan', 34.0224,118.2851, '11/20/2017');
